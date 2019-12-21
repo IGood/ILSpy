e54dce2e004fef07a48e428816927083c8d17e42
IGood <Ian.M.Good@gmail.com>
2019-12-20 22:50:06 -0800

    SharpTreeView project converted to Core 3.1

--- SharpTreeView/Properties/AssemblyInfo.cs
+++ SharpTreeView/Properties/AssemblyInfo.cs
@@ -2,8 +2,20 @@
 // This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
 
 using System.Reflection;
+using System.Resources;
 using System.Windows;
 using System.Windows.Markup;
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: AssemblyCompany("ic#code")]
+[assembly: AssemblyProduct("SharpDevelop")]
+[assembly: AssemblyCopyright("2000-2012 AlphaSierraPapa for the SharpDevelop Team")]
+[assembly: AssemblyVersion(RevisionClass.Major + "." + RevisionClass.Minor + "." + RevisionClass.Build + "." + RevisionClass.Revision)]
+[assembly: AssemblyInformationalVersion(RevisionClass.FullVersion + "-ca8a8e28")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly",
+	Justification = "AssemblyInformationalVersion does not need to be a parsable version")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -23,7 +35,6 @@ using System.Windows.Markup;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
 	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
 									 //(used if a resource is not found in the page, 
@@ -33,7 +44,16 @@ using System.Windows.Markup;
 											  // app, or any theme specific resource dictionaries)
 )]
 
-
 [assembly: XmlnsPrefix("http://icsharpcode.net/sharpdevelop/treeview", "treeview")]
-
 [assembly: XmlnsDefinition("http://icsharpcode.net/sharpdevelop/treeview", "ICSharpCode.TreeView")]
+
+internal static class RevisionClass
+{
+	public const string Major = "4";
+	public const string Minor = "2";
+	public const string Build = "0";
+	public const string Revision = "8752";
+	public const string VersionName = "Beta 2";
+
+	public const string FullVersion = Major + "." + Minor + "." + Build + ".8752-Beta 2";
+}
